f16858481400ae7c9e675f8fbb48b2da2c0c746d
Sofja Gordejeva <A250373SG@rvt.lv>
2026-05-28 16:00:59 +0300

    Pievienoju kodu javascripta un ccs

--- javascript.js
+++ javascript.js
@@ -1,2 +1,11 @@
+let secretCandy = Math.floor(Math.random() * 20);
 const board = document.getElementById('game-board');
- for (let i 
+ for (let i = 0; i < 20; i ++) {
+    const candy = document.createElement("button");
+    candy.textContent = (i + 1) + '🍬';
+    candy.dataset.id = i;
+    candy.addEventListener("Click", function() {
+        
+    })
+    board.appendChild(candy);
+ }
